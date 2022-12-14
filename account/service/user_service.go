69d691b248ba228a89ac1bf300f26c46408d7cfb
Nuriddin-Olimjon <Nuriddin.Obidjonov777@gmail.com>
2022-12-14 18:00:29 +0500

    ...

--- account/service/user_service.go
+++ account/service/user_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"log"
 
 	"github.com/Nuriddin-Olimjon/memrizr/account/model"
 	"github.com/google/uuid"
@@ -19,7 +20,25 @@ func (s *UserService) Get(ctx context.Context, uid uuid.UUID) (*model.User, erro
 }
 
 func (s *UserService) Signup(ctx context.Context, u *model.User) error {
-	panic("Method not implemented")
+	pw, err := hashPassword(u.Password)
+
+	if err != nil {
+		log.Printf("Unable to signup user for email: %v\n", u.Email)
+	}
+
+	u.Password = pw
+	if err := s.UserRepository.Create(ctx, u); err != nil {
+		return err
+	}
+
+	// If we get around to adding events, we'd Publish it here
+	// err := s.EventsBroker.PublishUserUpdated(u, true)
+
+	// if err != nil {
+	//  return nil, apperrors.NewInternal()
+	// }
+
+	return nil
 }
 
 // USConfig will hold repositories that will eventually be injected into this
